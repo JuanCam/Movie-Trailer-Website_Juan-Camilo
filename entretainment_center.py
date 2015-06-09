c1270092fa69c70af8373532819191e7b6dd940b
Juan Camilo <juancgr_4@hotmail.com>
2015-06-08 23:47:18 -0500

    Initial push

---
+++ entretainment_center.py
@@ -0,0 +1,62 @@
+import movie
+import fresh_tomatoes
+#All instances of the movie classes
+passion_of_the_christ = movie.Movie('Passion of the Christ',
+									'It starts in Gethsemane where Christ is praying, then comes his last hours, all he suffered, the crucifixion and his glorious resurrection according to Saint Matthew\'s gospel',
+									'http://upload.wikimedia.org/wikipedia/en/6/61/Thepassionposterface-1-.jpg',
+									'https://www.youtube.com/watch?v=MhdDIz3y9zo')
+
+guardians_of_the_galaxy = movie.Movie('Guardians of the Galaxy',
+									  'In 1988, following his mother\'s death, a young Peter Quill is abducted from Earth by the Ravagers, a group of space pirates led by Yondu Udonta. Twenty-six years later on the planet Morag, Quill steals an orb, after which Korath, a subordinate to the fanatical Kree, Ronan, intercepts him. Although Quill escapes with the orb, Yondu discovers his theft and issues a bounty for his capture, while Ronan sends the assassin Gamora after the orb.',
+									  'http://upload.wikimedia.org/wikipedia/en/8/8f/GOTG-poster.jpg',
+									  'https://www.youtube.com/watch?v=d96cjJhvlMA')
+
+san_andreas = movie.Movie('San Andreas',
+						  'Raymond \'Ray\' Gaines (Dwayne Johnson) is a Los Angeles Fire Department helicopter-rescue pilot. In the midst of a divorce from his estranged wife, Emma (Carla Gugino), he calls his daughter Blake (Alexandra Daddario) to discuss their upcoming trip to San Francisco. Meanwhile, Caltech seismologist Lawrence Hayes (Paul Giamatti) and his colleague Dr. Kim Park (Will Yun Lee) are at the Hoover Dam making a breakthrough in earthquake prediction when an unknown fault nearby ruptures, triggering a magnitude 7.1 earthquake that collapses the dam and kills Park in the process when he stays on the dam to save a little girl. When Ray is called into work, Blake reluctantly decides to go with her mother Emma\'s new boyfriend, Daniel Riddick (Ioan Gruffudd), a wealthy real estate developer, to San Francisco where he is completing a downtown high-rise building.',
+						  'http://upload.wikimedia.org/wikipedia/en/3/38/San_Andreas_poster.jpg',
+						  'https://www.youtube.com/watch?v=yftHosO0eUo')
+
+jurassic_park = movie.Movie('Jurassic Park',
+						    'John Hammond, the founder and CEO of bioengineering company InGen, has created a theme park called Jurassic Park on Isla Nublar, a tropical island in an isolated Central American location populated with cloned dinosaurs. ',
+						    'http://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg',
+						    'https://www.youtube.com/watch?v=lc0UehYemQA')
+aladdin = movie.Movie('Aladdin',
+					  'The film opens with Jafar, Grand Vizier to the Sultan of the fictional sultanate of Agrabah, attempting to retrieve a magical oil lamp containing a genie from the Cave of Wonders. He enlists a petty thief to enter the cave and retrieve it, whose attempt fails. Jafar and his parrot assistant, Iago, learn that only a "Diamond in the Rough" can enter the cave. Meanwhile, Jasmine, the Sultan\'s daughter, frustrated with her life in the palace, flees to Agrabah\'s marketplace. There she meets street rat Aladdin and his monkey pet, Abu, and the two begin to form a friendship. After Aladdin is arrested by orders of Jafar, Jasmine orders Jafar to have him released. Jafar lies to her that Aladdin was already executed, leaving Jasmine heartbroken.',
+					  'http://upload.wikimedia.org/wikipedia/en/5/58/Aladdinposter.jpg',
+					  'https://www.youtube.com/watch?v=_SHlwJmL1jY')
+
+prince_of_egypt = movie.Movie('Prince of Egypt',
+					  'In Ancient Egypt, Yocheved, a Hebrew slave, and her two children, Miriam and Aaron, watch as Hebrew babies are taken and slaughtered by Egyptian soldiers, as ordered by Seti I, who fears that an increase in Hebrew slaves could lead to rebellion. To save her own newborn son Moses, Yocheved places him in a basket afloat on the Nile. Miriam follows the basket to the Pharaoh\'s palace and witnesses her baby brother adopted by Pharaoh\'s queen. Twenty years later, Moses and his foster brother Rameses II are scolded by their father for accidentally destroying a temple during one of their youthful misadventures, though Moses tries to take the blame and says that Rameses wants their father\'s approval. That evening at a palace banquet, Seti, deciding to give Rameses this opportunity, names him Prince regent and gives him authority over Egypt\'s temples. As a tribute, the high priests Hotep and Huy offer him the captive Tzipporah, and Rameses gives her to Moses. Moses debunks Tzipporah, and Rameses appoints him Royal Chief Architect.',
+					  'http://upload.wikimedia.org/wikipedia/en/6/6c/Prince_of_egypt_ver2.jpg',
+					  'https://www.youtube.com/watch?v=yWs81poMgiM')
+
+avengers_1 = movie.Movie('The Avengers',
+					  'The Asgardian Loki encounters the Other, the leader of an extraterrestrial race known as the Chitauri. In exchange for retrieving the Tesseract,2 a powerful energy source of unknown potential, the Other promises Loki an army with which he can subjugate Earth. Nick Fury, director of the espionage agency S.H.I.E.L.D., and his lieutenant Agent Maria Hill arrive at a remote research facility during an evacuation, where physicist Dr. Erik Selvig is leading a research team experimenting on the Tesseract. Agent Phil Coulson explains that the object has begun radiating an unusual form of energy. The Tesseract suddenly activates and opens a wormhole, allowing Loki to reach Earth. Loki takes the Tesseract and uses his scepter to enslave Selvig and several agents, including Clint Barton, to aid him in his getaway.',
+					  'http://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg',
+					  'https://www.youtube.com/watch?v=eOrNdBpGMv8')
+avengers_2 = movie.Movie('Avengers age of ultron',
+						 'A worldwide backlash over the resulting destruction, and the fears Wanda\'s hallucinations incited, send the team into hiding at Barton\'s safehouse. Thor departs to consult with Dr. Erik Selvig on the meaning of the apocalyptic future he saw in his hallucination, while Romanoff and Banner plan to flee together after realizing a mutual attraction. However, Nick Fury arrives and encourages the team to form a plan to stop Ultron. In Seoul, Ultron forces the team\'s friend Dr. Helen Cho to use her synthetic-tissue technology, together with vibranium and the scepter\'s gem, to perfect a new body for him. As Ultron uploads himself into the body, Wanda is able to read his mind; discovering his plan for human extinction, the Maximoffs turn on Ultron. Rogers, Romanoff, and Barton find Ultron and retrieve the synthetic body, but Ultron captures Romanoff.',
+						 'http://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg',
+						 'https://www.youtube.com/watch?v=JAUoeqvedMo')
+
+iron_man = movie.Movie('Iron Man',
+					   'Iron Man (Tony Stark) is a fictional superhero appearing in American comic books published by Marvel Comics, as well as its associated media. The character was created by writer-editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby. He made his first appearance in Tales of Suspense #39 (cover dated March 1963).',
+					   'http://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG',
+					   'https://www.youtube.com/watch?v=8hYlB38asDY')
+as_good_as_it_gets = movie.Movie('As Good as it Gets',
+								 'Melvin Udall is a misanthrope who works at home as a best-selling novelist in New York City. He suffers from obsessive-compulsive disorder which, paired with his misanthropy, alienates nearly everyone with whom he interacts. He avoids stepping on sidewalk cracks while walking through the city due to a superstition of bad luck, and eats breakfast at the same table in the same restaurant every day using disposable plastic utensils he brings with him due to his pathological mysophobia. He takes an interest in his waitress, Carol Connelly, the only server at the restaurant who can tolerate his behavior.',
+								 'http://upload.wikimedia.org/wikipedia/en/d/dc/As_good_as_it_gets.jpg',
+								 'https://www.youtube.com/watch?v=rrRl2QQKkI8')
+
+#passion_of_the_christ.show_trailer()
+#pass the instances to the fresh_tomates file for constructing the html page
+fresh_tomatoes.open_movies_page([passion_of_the_christ,
+								 guardians_of_the_galaxy,
+								 san_andreas,
+								 jurassic_park,
+								 aladdin,
+								 prince_of_egypt,
+								 avengers_1,
+								 iron_man,
+								 as_good_as_it_gets,
+								 avengers_2])
